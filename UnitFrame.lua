cf91c09de486ac13f58669e6364515a137265f73
Genimal <30530063+Genimal@users.noreply.github.com>
2018-07-06 14:00:08 +0900

    WOW-26970patch8.0.1_Beta

--- UnitFrame.lua
+++ UnitFrame.lua
@@ -1,7 +1,11 @@
-﻿local n,w,h="CompactUnitFrameProfilesGeneralOptionsFrame" h,w=_G[n.."HeightSlider"],_G[n.."WidthSlider"] h:SetMinMaxValues(40,45) w:SetMinMaxValues(40,45) -- 공격대창 크기
+-- 공격대창 크기
+local CUFOF="CompactUnitFrameProfilesGeneralOptionsFrame";
+_G[CUFOF.."HeightSlider"]:SetMinMaxValues(40,45);
+_G[CUFOF.."WidthSlider"]:SetMinMaxValues(40,45);
 
+-- 기본프레임 파티숫자 숨기기+프레임 붙이기
 hooksecurefunc("CompactRaidGroup_UpdateLayout",function(f)
-f.title:SetHeight(0);f.title:SetText()
+f.title:SetHeight(0);f.title:SetWidth(0);f.title:SetText(nil)
 local f1 = _G[f:GetName().."Member1"]
 f1:ClearAllPoints()
 	if (CUF_HORIZONTAL_GROUPS)then 
@@ -11,11 +15,11 @@ f1:ClearAllPoints()
 		f1:SetPoint("TOP")
 		f:SetWidth(f1:GetWidth())
 	end
-end) -- 기본프레임 파티숫자 숨기기+프레임 붙이기
+end)
 
---hooksecurefunc("CompactPartyFrame_Generate", function() CompactPartyFrameTitle:SetText(nil);CompactPartyFrameTitle:SetHeight(0) end)
---hooksecurefunc("CompactRaidGroup_UpdateUnits", function(frame) frame.title:SetText(nil);frame.title:SetHeight(0) end)
-
--- local cuf;hooksecurefunc("CompactUnitFrame_UpdateHealPrediction", function(cuf) if cuf:GetName():find("NamePlate%d") then return end;cuf.OverAbsorbGlow:SetAlpha(0.3);end)
-
---hooksecurefunc("CompactRaidGroup_UpdateLayout",function(f)f.title:SetHeight(0);local f1 = _G[f:GetName().."Member1"];f1:ClearAllPoints()if (CUF_HORIZONTAL_GROUPS)then f1:SetPoint("TOPLEFT");f:SetHeight(f1:GetHeight())else f1:SetPoint("TOP");f:SetWidth(f1:GetWidth())end;end)
+-- 전장 입장시 내가 공격대장일 경우 자동으로 전원 부공 승급
+hooksecurefunc("RaidFrameAllAssistCheckButton_UpdateAvailable", function(self)
+		if select(2,IsInInstance())=="pvp" and (UnitIsGroupLeader("player"))and not IsEveryoneAssistant() then
+			SetEveryoneIsAssistant(true);
+		end;
+	end)
